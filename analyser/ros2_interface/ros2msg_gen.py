3b81dc70346eecfc81771651811ca7d0d9c5d2d6
AmirInt <amir.intelli@gmail.com>
2024-09-15 16:38:07 +0330

    refactor: use only a message object inside NetworkStatus

--- analyser/ros2_interface/ros2msg_gen.py
+++ analyser/ros2_interface/ros2msg_gen.py
@@ -23,7 +23,6 @@ from rosbags.typesys.types import dm_network_info_msgs__msg__NetworkStatus as Ne
 from rosbags.typesys.types import dm_network_info_msgs__msg__ObjectInfoN as ObjectInfoNMsg
 from rosbags.typesys.types import dm_network_info_msgs__msg__FreespaceInfoN as FreespaceInfoNMsg
 from rosbags.typesys.types import dm_network_info_msgs__msg__SignalInfoN as SignalInfoNMsg
-from datetime import datetime
 from csv_interface import dm_interface
 
 
@@ -45,45 +44,36 @@ class NetworkStatus:
             packet_count,
             time_stamp,
             medium):
-        self.delay = delay
-        self.jitter = jitter
-        self.rssi = rssi
-        self.packet_loss = packet_loss
-        self.packet_count = packet_count
-        if not (medium == 0 or medium == 1):
-            raise Exception(f"Network status medium input invalid: {medium}")
-        self.medium = medium 
-        self.ros_time_stamp = TimeStamp(time_stamp).ros_time_stamp
-        self.network_status = NetworkStatusMsg(
-            delay=delay,
-            jitter=jitter,
-            rssi=rssi,
-            packet_loss=packet_loss,
-            packet_count=packet_count,
-            stamp=self.ros_time_stamp,
-            medium=medium)
+        self.msg = NetworkStatusMsg()
+        self.msg.delay = delay
+        self.msg.jitter = jitter
+        self.msg.rssi = rssi
+        self.msg.packet_loss = packet_loss
+        self.msg.packet_count = packet_count
+        self.msg.medium = medium 
+        self.msg.stamp = TimeStamp(time_stamp).ros_time_stamp
 
 
 class ObjectInfo:
-    def __init__(self, csv_row):
-        self.csv_row = csv_row
+    def __init__(self, dataframe_row):
+        self.dataframe_row = dataframe_row
         self.__build_msg()
     
     def __build_msg(self):
         self.msg = ObjectInfoMsg()
-        self.msg.id.value = dm_interface.get_object_id(self.csv_row)
-        self.msg.time.value = dm_interface.get_object_timestamp_its(self.csv_row)
+        self.msg.id.value = dm_interface.get_object_id(self.dataframe_row)
+        self.msg.time.value = dm_interface.get_object_timestamp_its(self.dataframe_row)
         object_class = ObjectClass()
-        object_class.id.value = dm_interface.get_object_class_id(self.csv_row)
-        object_class.confidence.value = dm_interface.get_object_class_confidence(self.csv_row)
+        object_class.id.value = dm_interface.get_object_class_id(self.dataframe_row)
+        object_class.confidence.value = dm_interface.get_object_class_confidence(self.dataframe_row)
         self.msg.object_class.append(object_class)
-        self.msg.existency.value = dm_interface.get_class_existence_confidence(self.csv_row)
-        self.msg.object_location.geodetic_system.value = dm_interface.get_object_geodetic_system(self.csv_row)
-        self.msg.object_location.latitude.value = dm_interface.get_object_latitude(self.csv_row)
-        self.msg.object_location.longitude.value = dm_interface.get_object_longitude(self.csv_row)
-        self.msg.object_location.altitude.value = dm_interface.get_object_altitude(self.csv_row)
-        self.msg.orientation.value = dm_interface.get_object_orientation(self.csv_row)
-        object_size = dm_interface.get_object_size(self.csv_row)
+        self.msg.existency.value = dm_interface.get_class_existence_confidence(self.dataframe_row)
+        self.msg.object_location.geodetic_system.value = dm_interface.get_object_geodetic_system(self.dataframe_row)
+        self.msg.object_location.latitude.value = dm_interface.get_object_latitude(self.dataframe_row)
+        self.msg.object_location.longitude.value = dm_interface.get_object_longitude(self.dataframe_row)
+        self.msg.object_location.altitude.value = dm_interface.get_object_altitude(self.dataframe_row)
+        self.msg.orientation.value = dm_interface.get_object_orientation(self.dataframe_row)
+        object_size = dm_interface.get_object_size(self.dataframe_row)
         self.msg.size.length.value.value = object_size[0]
         self.msg.size.width.value.value = object_size[1]
         self.msg.size.height.value.value = object_size[2]
@@ -93,19 +83,28 @@ class ObjectInfo:
         object_info_array = ObjectInfoArray(array=object_infos)
         return object_info_array
 
+    @staticmethod
+    def create_object_info_array_n(
+            object_info_array: ObjectInfoArray,
+            network_status: NetworkStatus) -> ObjectInfoNMsg:
+        object_info_n = ObjectInfoNMsg(
+            object_info_array=object_info_array,
+            network_status=network_status)
+        return object_info_n
+
 
 class SignalInfo:
-    def __init__(self, csv_row):
-        self.csv_row = csv_row
+    def __init__(self, dataframe_row):
+        self.dataframe_row = dataframe_row
         self.__build_msg()
     
     def __build_msg(self):
         self.msg = SignalInfoMsg()
-        self.msg.crp_id.value = dm_interface.get_signal_crp_id(self.csv_row)
+        self.msg.crp_id.value = dm_interface.get_signal_crp_id(self.dataframe_row)
         self.msg.signal_id_list.append(
-            [SignalId(value=beacon_id) for beacon_id in dm_interface.get_signal_beacon_id(self.csv_row)])
-        self.msg.time.value = dm_interface.get_signal_timestamp_its(self.csv_row)
-        signal_light_info = dm_interface.get_signal_light_info(self.csv_row)
+            [SignalId(value=beacon_id) for beacon_id in dm_interface.get_signal_beacon_id(self.dataframe_row)])
+        self.msg.time.value = dm_interface.get_signal_timestamp_its(self.dataframe_row)
+        signal_light_info = dm_interface.get_signal_light_info(self.dataframe_row)
         signal_light_info_msg = SignalLightInfo()
         signal_light_info_msg.main_light.value = signal_light_info[0]
         signal_light_info_msg.arrow_light.value = signal_light_info[1]
@@ -118,18 +117,27 @@ class SignalInfo:
         signal_info_array = SignalInfoArray(array=signal_infos)
         return signal_info_array
     
+    @staticmethod
+    def create_signal_info_array_n(
+            signal_info_array: SignalInfoArray,
+            network_status: NetworkStatus) -> SignalInfoNMsg:
+        signal_info_n = SignalInfoNMsg(
+            signal_info_array=signal_info_array,
+            network_status=network_status)
+        return signal_info_n
+
 
 class FreespaceInfo:
-    def __init__(self, csv_row):
-        self.csv_row = csv_row
+    def __init__(self, dataframe_row):
+        self.dataframe_row = dataframe_row
         self.__build_msg()
     
     def __build_msg(self):
         self.msg = FreespaceInfoMsg()
-        self.msg.id.value = dm_interface.get_object_id(self.csv_row)
-        self.msg.time.value = dm_interface.get_object_timestamp_its(self.csv_row)
-        position_begin = dm_interface.get_freespace_position_begin(self.csv_row)
-        position_end = dm_interface.get_freespace_position_end(self.csv_row)
+        self.msg.id.value = dm_interface.get_object_id(self.dataframe_row)
+        self.msg.time.value = dm_interface.get_object_timestamp_its(self.dataframe_row)
+        position_begin = dm_interface.get_freespace_position_begin(self.dataframe_row)
+        position_end = dm_interface.get_freespace_position_end(self.dataframe_row)
         self.msg.position_begin.geodetic_system.value = position_begin[0]
         self.msg.position_begin.latitude.value = position_begin[1]
         self.msg.position_begin.longitude.value = position_begin[2]
@@ -143,3 +151,12 @@ class FreespaceInfo:
     def create_freespace_info_array(freespace_infos: list[FreespaceInfoMsg]) -> FreespaceInfoArray:
         freespace_info_array = FreespaceInfoArray(array=freespace_infos)
         return freespace_info_array
+    
+    @staticmethod
+    def create_freespace_info_array_n(
+            freespace_info_array: FreespaceInfoArray,
+            network_status: NetworkStatus) -> FreespaceInfoNMsg:
+        freespace_info_n = FreespaceInfoNMsg(
+            freespace_info_array=freespace_info_array,
+            network_status=network_status)
+        return freespace_info_n
